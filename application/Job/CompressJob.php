4af8e28e5db43909c19e0d205541e439991fb82d
david7207 <david7207@sina.com>
2019-09-08 18:23:48 +0800

    fix

--- application/Job/CompressJob.php
+++ application/Job/CompressJob.php
@@ -17,7 +17,7 @@ class CompressJob extends JobBase {
             $percent = 1;  #原图压缩，不缩放，但体积大大降低
             //echo $root . $source;
             (new CompressService($source, $percent))->compressImg($source);
-            echo '压缩图片-' . $picInfo['pathname'] . '完成' . PHP_EOL;
+            echo '压缩图片 - ' . $picInfo['pathname'] . '完成' . PHP_EOL;
         } catch (Exception $exception) {
             (new QueueLog())->insert(['queue_name' => self::class, 'job_data' => json_encode($this->jobData), 'error_msg' => $exception->getMessage()]);
             echo '压缩图片脚本出现异常，异常消息为：' . $exception->getMessage() . PHP_EOL;
